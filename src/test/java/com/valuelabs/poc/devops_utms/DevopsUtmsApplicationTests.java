1117148d5911f12f3d4754a7dc2d24c65d656a27
Madhu Alishala <malishala@TLP.ads.valuelabs.net>
2018-05-28 13:33:20 +0530

    removed test case logic and added new unit test cases

--- src/test/java/com/valuelabs/poc/devops_utms/DevopsUtmsApplicationTests.java
+++ src/test/java/com/valuelabs/poc/devops_utms/DevopsUtmsApplicationTests.java
@@ -1,5 +1,7 @@
 package com.valuelabs.poc.devops_utms;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,9 +10,42 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DevopsUtmsApplicationTests {
-
-	@Test
-	public void contextLoads() {
+	
+	public int add(int a,int b){
+		return a+b;
 	}
+	
+	/*public int divide(int a,int b){
+		return a/b;
+	}*/
+	
+
+	/*@Test
+	public void contextLoads() {
+		
+	}*/
+	
+	@Test
+	public void getPositiveNumber(){
+		
+		assertEquals(2, add(1,1));
+	}
+	
+	@Test
+	public void getNegativeNumber(){
+		assertEquals(-2, add(-1,-1));
+	}
+	
+	@Test
+	public void getNegativeResult(){
+		assertEquals(-1, add(-2,1));
+	}
+	
+	@Test
+	public void getPositiveResult(){
+		assertEquals(1, add(2,-1));
+	}
+	
+	
 
 }
